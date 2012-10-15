b39895f6e2cc96f840a98111415e0ca83a9166f5
Huiming Teo <teohuiming@gmail.com>
2012-10-15 21:04:59 +0800

    update percent notation

--- spec/percent_notation_spec.rb
+++ spec/percent_notation_spec.rb
@@ -1,4 +1,4 @@
-require 'minitest/spec'
+require 'minitest/autorun'
 
 # See also:
 # http://en.wikibooks.org/wiki/Ruby_Programming/Syntax/Literals#The_.25_Notation
@@ -13,21 +13,32 @@ describe "The % Notation" do
       %%test 50\%%.must_equal "test 50%"
       %'test \'test'.must_equal "test 'test"
     end
-    it "allows unescaped pairing () [] {} <>" do
-      %(test (test)).must_equal "test (test)"
-      %{tes{t} \{test}.must_equal "tes{t} {test"
-      %[tes[t] test\]].must_equal "tes[t] test]"
+    it "allows unescaped pair of () [] {} <>" do
+      %(test (nested (test))).must_equal "test (nested (test))"
+      %{tes{t} \{test\}}.must_equal "tes{t} {test}"
+      %[tes[t] \[test\]].must_equal "tes[t] \[test]"
       %<tes<t> test\>>.must_equal "tes<t> test>"
     end
   end
 
-  describe "modifier" do
+  describe "modifiers" do
+    it "Overview" do
+      %(interpolated string (#{ "default" })).must_equal 'interpolated string (default)'
+      %Q(interpolated string (#{ "default" })).must_equal 'interpolated string (default)'
+      %q(non-interpolated string).must_equal 'non-interpolated string'
+      %r(#{ "interpolated" } regexp)i.must_equal /interpolated regexp/i
+      %w(non-interpolated\ string  separated\ by\ whitespaces).must_equal ['non-interpolated string', 'separated by whitespaces']
+      %W(interpolated\ string #{ "separated by whitespaces" }).must_equal ['interpolated string', 'separated by whitespaces']
+      %s(non-interpolated symbol).must_equal :'non-interpolated symbol'
+      %x(echo #{ "interpolated shell command" }).must_equal "interpolated shell command\n"
+    end
+
     it "Q - interpolated string (default)" do
       %(hi#{ 1 + 4 }).must_equal "hi5"
       %Q(hi#{ 1 + 4 }).must_equal "hi5"
     end
     it "q - non-interpolated string" do
-      %q(foo #{bar}).must_equal 'foo #{bar}'
+      %q(foo\n #{bar}).must_equal 'foo\n #{bar}'
     end
     it "r - interpolated regexp" do
       '/path/to/MARS'.must_match %r(/path/to/mars)i  # regex flags after delimiter
